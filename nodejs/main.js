b19660008dbd976e82ffda32d66466ce025adee7
myjung2620 <myjung2620@gmail.com>
2018-12-28 00:58:08 +0100

    nodejs express hello world

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,89 +1,27 @@
-var http = require('http');
-var url = require('url');
-var qs = require('querystring');
+var express = require('express')
+var app = express()
+var fs = require('fs');
 var template = require('./lib/template.js');
-var db = require('./lib/db');
-var topic = require('./lib/topic');
  
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-    if(pathname === '/'){
-      if(queryData.id === undefined){
-        topic.home(request, response);
-      } else {
-        topic.page(request, response);
-      }
-    } else if(pathname === '/create'){
-      topic.create(request, response);
-    }  else if(pathname === '/create_process'){
-      topic.create_process(request, response);
-    } else if(pathname === '/update'){
-      db.query('SELECT * FROM topic', function(error, topics){
-        if(error){
-          throw error;
-        }
-        db.query(`SELECT * FROM topic WHERE id=?`,[queryData.id], function(error2, topic){
-          if(error2){
-            throw error2;
-          }
-          db.query('SELECT * FROM author', function(error2, authors){
-            var list = template.list(topics);
-            var html = template.HTML(topic[0].title, list,
-              `
-              <form action="/update_process" method="post">
-                <input type="hidden" name="id" value="${topic[0].id}">
-                <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
-                <p>
-                  <textarea name="description" placeholder="description">${topic[0].description}</textarea>
-                </p>
-                <p>
-                  ${template.authorSelect(authors, topic[0].author_id)}
-                </p>
-                <p>
-                  <input type="submit">
-                </p>
-              </form>
-              `,
-              `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
-            );
-            response.writeHead(200);
-            response.end(html);
-          });
-           
-        });
-      });
-    } else if(pathname === '/update_process'){
-      var body = '';
-      request.on('data', function(data){
-          body = body + data;
-      });
-      request.on('end', function(){
-          var post = qs.parse(body);
-          db.query('UPDATE topic SET title=?, description=?, author_id=? WHERE id=?', [post.title, post.description, post.author, post.id], function(error, result){
-            response.writeHead(302, {Location: `/?id=${post.id}`});
-            response.end();
-          })
-      });
-    } else if(pathname === '/delete_process'){
-      var body = '';
-      request.on('data', function(data){
-          body = body + data;
-      });
-      request.on('end', function(){
-          var post = qs.parse(body);
-          db.query('DELETE FROM topic WHERE id = ?', [post.id], function(error, result){
-            if(error){
-              throw error;
-            }
-            response.writeHead(302, {Location: `/`});
-            response.end();
-          });
-      });
-    } else {
-      response.writeHead(404);
-      response.end('Not found');
-    }
+//route, routing
+//app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function(request, response) { 
+  fs.readdir('./data', function(error, filelist){
+    var title = 'Welcome';
+    var description = 'Hello, Node.js';
+    var list = template.list(filelist);
+    var html = template.HTML(title, list,
+      `<h2>${title}</h2>${description}`,
+      `<a href="/create">create</a>`
+    ); 
+    response.send(html);
+  });
 });
-app.listen(3000);
+ 
+app.get('/page', function(req, res) { 
+  return res.send('/page');
+});
+ 
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!')
+});
